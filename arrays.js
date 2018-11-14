770714760e006ccd24eee8f1dcf8d9b8a5424a8d
Raheem <raheemtheallstar137@yahoo.com>
2018-11-14 18:49:37 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(array,element){
+  var change = array.unshift(element);
+  return change;
+}
+function destructivelyAddElementToBeginningOfArray(array[],element){
+
+}
+function addElementToEndOfArray(){}
