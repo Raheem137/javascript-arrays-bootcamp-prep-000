fe8304b51284fda561093b031172ab8d6a3eb0ea
Raheem <raheemtheallstar137@yahoo.com>
2018-11-14 19:00:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(array,element){
  var change = array.unshift(element);
-
+ return change;
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
 
