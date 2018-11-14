1d4beb4f1e1d6c01496d866574c8e7aef92c4ef1
Raheem <raheemtheallstar137@yahoo.com>
2018-11-14 19:07:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,4 +8,4 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 
 }
 function addElementToEndOfArray(){}
-console.log(addElementToBeginningOfArray(chocolateBars,word));
+console.log(chocolateBars);
